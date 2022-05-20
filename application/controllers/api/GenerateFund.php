a7ba6f17e3f6c9ecfec5273d42e59f42a1a4c2ab
NitaiKOIRI463 <55233409+iotasonl@users.noreply.github.com>
2022-05-20 15:23:44 +0530

    generate fund module

--- application/controllers/api/GenerateFund.php
+++ application/controllers/api/GenerateFund.php
@@ -16,16 +16,102 @@ class GenerateFund extends REST_Controller
         if($this->input->post('package_id',true)=='')
         {
             $this->response(['status'=>false,'data'=>[],'msg'=>'package_id required !','response_code' => REST_Controller::HTTP_BAD_REQUEST]);
-        }elseif($this->input->post('qty',true))
+        }elseif($this->input->post('qty',true)=='')
         {
-            $this->response(['status'=>false,'data'=>[],'msg'=>'member_id required !','response_code' => REST_Controller::HTTP_BAD_REQUEST]);
-        }elseif($this->input->post('c_by',true))
+            $this->response(['status'=>false,'data'=>[],'msg'=>'qty required !','response_code' => REST_Controller::HTTP_BAD_REQUEST]);
+        }elseif($this->input->post('c_by',true)=='')
         {
-            $this->response(['status'=>false,'data'=>[],'msg'=>'member_id required !','response_code' => REST_Controller::HTTP_BAD_REQUEST]);
-        }
-   }
-   
+            $this->response(['status'=>false,'data'=>[],'msg'=>'c_by required !','response_code' => REST_Controller::HTTP_BAD_REQUEST]);
+        }else
+        {
+            $package_id = $this->GenerateFund_model->verifyPackageExist_m($this->input->post('package_id',true));
+            if($package_id>0)
+            {
 
+                $package_amount = $this->GenerateFund_model->getPackageAmount_m($this->input->post('package_id',true));
+
+                $package_available_exist = $this->GenerateFund_model->verifyPackageAlreadyGeneareted_m($this->input->post('package_id',true));
+                if($package_available_exist>0)
+                {
+                    $package_total_fund = $this->GenerateFund_model->getPackageFund_m($this->input->post('package_id',true));
+                    $mainData = [];
+                    $whereData = [];
+                    $whereData['package_id'] = $this->input->post('package_id',true);
+                    $mainData['total_fund'] = $package_total_fund+($package_amount*$this->input->post('qty',true));
+                    $mainData['d_by'] = $this->input->post('c_by',true);
+                    $mainData['d_date'] = date('Y-m-d H:i:s');
+                    $mainData['status'] = 1;
+                    $this->GenerateFund_model->updateGenerateFundAvailable_m($mainData,$whereData);
+
+                    $mainHistory = [];
+                    $mainHistory['package_id'] = $this->input->post('package_id',true);
+                    $mainHistory['qty'] = $this->input->post('qty',true);
+                    $mainHistory['amount'] = $package_amount;
+                    $mainHistory['total'] = ($package_amount*$this->input->post('qty',true));
+                    $mainHistory['c_date'] = date('Y-m-d H:i:s');
+                    $mainHistory['c_by'] = $this->input->post('c_by',true);
+                    $mainHistory['status'] = 1;
+                    $mainHistory['transaction_type'] = 'IN';
+                    $this->GenerateFund_model->generateFundHistories_m($mainHistory);
+                    $this->response(['status'=>false,'data'=>[],'msg'=>'Successfully Fund Added !','response_code' => REST_Controller::HTTP_OK]);
+                }else
+                {
+                    $mainData = [];
+                    $mainData['package_id'] = $this->input->post('package_id',true);
+                    $mainData['total_fund'] = ($package_amount*$this->input->post('qty',true));
+                    $mainData['d_by'] = $this->input->post('c_by',true);
+                    $mainData['d_date'] = date('Y-m-d H:i:s');
+                    $mainData['status'] = 1;
+                    $this->GenerateFund_model->insertGenerateFundAvailable_m($mainData);
+
+                    $mainHistory = [];
+                    $mainHistory['package_id'] = $this->input->post('package_id',true);
+                    $mainHistory['qty'] = $this->input->post('qty',true);
+                    $mainHistory['amount'] = $package_amount;
+                    $mainHistory['total'] = ($package_amount*$this->input->post('qty',true));
+                    $mainHistory['c_date'] = date('Y-m-d H:i:s');
+                    $mainHistory['c_by'] = $this->input->post('c_by',true);
+                    $mainHistory['status'] = 1;
+                    $mainHistory['transaction_type'] = 'IN';
+                    $this->GenerateFund_model->generateFundHistories_m($mainHistory);
+                    $this->response(['status'=>false,'data'=>[],'msg'=>'Successfully Fund Added !','response_code' => REST_Controller::HTTP_OK]);
+                }
+
+            }else
+            {
+                $this->response(['status'=>false,'data'=>[],'msg'=>'Invalid package Id !','response_code' => REST_Controller::HTTP_BAD_REQUEST]);
+            }
+        }
+
+   }
+
+   public function get_generate_fund_history_post()
+   {
+        $package_id = $this->input->post('package_id',true)!=''?$this->input->post('package_id',true):'';
+        $resultData = $this->GenerateFund_model->getGeneratedFundHistory_m($package_id);
+        if(!empty($resultData))
+        {
+            $this->response(['status'=>false,'data'=>['histories'=>$resultData],'msg'=>'Successfully Fetched !','response_code' => REST_Controller::HTTP_OK]);
+        }else
+        {
+            $this->response(['status'=>false,'data'=>['histories'=>$resultData],'msg'=>'No Record Found !','response_code' => REST_Controller::HTTP_OK]);
+        }
+        
+   }
+
+   public function get_available_fund_post()
+   {
+        $package_id = $this->input->post('package_id',true)!=''?$this->input->post('package_id',true):'';
+        $resultData = $this->GenerateFund_model->getAvailableFunds_m($package_id);
+        if(!empty($resultData))
+        {
+            $this->response(['status'=>false,'data'=>['package_fund'=>$resultData],'msg'=>'Successfully Fetched !','response_code' => REST_Controller::HTTP_OK]);
+        }else
+        {
+            $this->response(['status'=>false,'data'=>['package_fund'=>$resultData],'msg'=>'No Record Found !','response_code' => REST_Controller::HTTP_OK]);
+        }
+        
+   }
 
 	
 }
--- application/models/GenerateFund_model.php
+++ application/models/GenerateFund_model.php
@@ -10,6 +10,95 @@ class GenerateFund_model extends CI_Model
   }
 
 
+  public function verifyPackageAlreadyGeneareted_m($package_id)
+  {
+      $result = $this->db->select('count(id) as total ')->from('tbl_package_available_fund')->where(['status'=>1,'package_id'=>$package_id])->get()->result_array();
+      if(!empty($result))
+      {
+         return $result[0]['total'];
+      }else
+      {
+         return 0;
+      }
+  }
+
+
+  public function verifyPackageExist_m($package_id)
+  {
+      $result = $this->db->select('count(id) as total ')->from('tbl_plan_master')->where(['status'=>1,'package_id'=>$package_id])->get()->result_array();
+      if(!empty($result))
+      {
+         return $result[0]['total'];
+      }else
+      {
+         return 0;
+      }
+  }
+
+  public function getPackageAmount_m($package_id)
+  {
+      $result = $this->db->select('package_amount')->from('tbl_plan_master')->where(['status'=>1,'package_id'=>$package_id])->get()->result_array();
+      if(!empty($result))
+      {
+         return $result[0]['package_amount'];
+      }else
+      {
+         return 0;
+      }
+      
+  }
+
+  public function getPackageFund_m($package_id)
+  {
+      $result = $this->db->select('total_fund')->from('tbl_package_available_fund')->where(['status'=>1,'package_id'=>$package_id])->get()->result_array();
+      if(!empty($result))
+      {
+         return $result[0]['total_fund'];
+      }else
+      {
+         return 0;
+      }
+      
+  }
+
+  public function generateFundHistories_m($data)
+  {
+      return $this->db->insert('tbl_generate_fund_histories',$data);
+  }
+
+  public function updateGenerateFundAvailable_m($data,$where)
+  {
+      return $this->db->update('tbl_package_available_fund',$data,$where);
+  }
+
+  public function insertGenerateFundAvailable_m($data)
+  {
+      return $this->db->insert('tbl_package_available_fund',$data);
+  }
+
+  public function getGeneratedFundHistory_m($package_id)
+  {
+        if($package_id!="")
+               $this->db->where(['h.package_id'=>$package_id]);
+
+         return $this->db->select('p.package_name,h.package_id,h.qty,h.amount,h.total,DATE_FORMAT(h.c_date,"%d-%m-%Y %H:%i:%s") as gen_date,h.transaction_type')
+         ->from('tbl_generate_fund_histories h')
+         ->join('tbl_plan_master p','p.package_id = h.package_id','left')
+         ->where(['h.status'=>1])->get()->result_array();
+  }
+
+  public function getAvailableFunds_m($package_id)
+  {
+        if($package_id!="")
+               $this->db->where(['h.package_id'=>$package_id]);
+
+         return $this->db->select('p.package_name,h.package_id,h.total_fund,DATE_FORMAT(h.d_date,"%d-%m-%Y %H:%i:%s") as last_update_date')
+         ->from('tbl_package_available_fund h')
+         ->join('tbl_plan_master p','p.package_id = h.package_id','left')
+         ->where(['h.status'=>1])->get()->result_array();
+  }
+
+
 
 
 }
